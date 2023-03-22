8b6d7fb398a48b1918b67b157bd8a3a2ffab8928
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2023-03-21 22:29:03 -0300

    chore: config ssr in stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { styled } from "../styles"
 
-const inter = Inter({ subsets: ['latin'] })
+ const Button = styled('button', {
+   backgroundColor: '$rocketseat',
+   borderRadius: 4,
+   border: 0,
+   padding: '4px 8px',
 
-export default function Home() {
-  return (
-    <>
-      <h1>Hello world</h1>
+   span: {
+     fontWeight: 'bold'
+   },
+
+   '&:hover': {
+     filter: 'brightness(0.8)'
+   }
+ })
+
+ export default function Home() {
+   return (
+    <>     
+      <h1>Hello World</h1>
+      <Button>
+        <span>Testedassd</span>
+        Enviar
+      </Button>
     </>
-  )
-}
+   )
+ }
