355c302c9a597ac151a18d5d816e9be1e94df758
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2023-08-23 18:05:58 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,30 @@
-import { styled } from "../styles"
+import Image from "next/image"
 
- const Button = styled('button', {
-   backgroundColor: '$green300',
-   borderRadius: 4,
-   border: 0,
-   padding: '4px 8px',
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
+import { HomeContainer, Product } from "../styles/pages/home"
 
-   span: {
-     fontWeight: 'bold'
-   },
+export default function Home() {
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
 
-   '&:hover': {
-     filter: 'brightness(0.8)'
-   }
- })
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
 
- export default function Home() {
-   return (
-    <>     
-      <h1>Hello World</h1>
-      <Button>
-        <span>Testedassd</span>
-        Enviar
-      </Button>
-    </>
-   )
- }
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
+}
