bb46e3f35db7b8226c878d2fc9546fb859926603
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2023-08-29 13:19:18 -0300

    feat: validate session_id parameter

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -34,22 +34,31 @@ export default function Success({ costumerName, product }: SuccessProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-    const sessionId = String(query.session_id);
-  
-    const session = await stripe.checkout.sessions.retrieve(sessionId, {
-      expand: ['line_items', 'line_items.data.price.product']
-    });
-  
-    const costumerName = session.customer_details?.name;
-    const product = session.line_items?.data[0].price?.product as Stripe.Product;
-  
+  if (!query.session_id) {
     return {
-      props: {
-        costumerName,
-        product: {
-          name: product.name,
-          imageUrl: product.images[0]
-        }
+      redirect: {
+        destination: '/',
+        permanent: false,
       }
     }
   }
+
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product']
+  });
+
+  const costumerName = session.customer_details?.name;
+  const product = session.line_items?.data[0].price?.product as Stripe.Product;
+
+  return {
+    props: {
+      costumerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0]
+      }
+    }
+  }
+}
